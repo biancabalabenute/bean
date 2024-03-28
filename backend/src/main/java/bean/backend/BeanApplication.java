211aaf0808380e94ea13386c53ddf1be52d5a202
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-03-28 20:39:20 -0300

    instanciando o backend

---
+++ backend/src/main/java/bean/backend/BeanApplication.java
@@ -0,0 +1,13 @@
+package bean.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanApplication.class, args);
+	}
+
+}
