4cbf824ff07142acef3583b2611ec26b629f9a01
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-03-28 23:07:01 -0300

    criando pedidoRepository

---
+++ backend/src/main/java/bean/backend/repository/PedidoRepository.java
@@ -0,0 +1,10 @@
+package bean.backend.repository;
+
+import bean.backend.entities.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+
+}
