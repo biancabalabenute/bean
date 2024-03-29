b4061e0e316b4488847450adef693285fd8482ab
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-03-28 23:20:38 -0300

    adicionando pedido no testConfig

--- backend/src/main/java/bean/backend/config/TestConfig.java
+++ backend/src/main/java/bean/backend/config/TestConfig.java
@@ -1,12 +1,15 @@
 package bean.backend.config;
 
 import bean.backend.entities.Cliente;
+import bean.backend.entities.Pedido;
 import bean.backend.repository.ClienteRepository;
+import bean.backend.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -16,12 +19,20 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private ClienteRepository clienteRepository;
 
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
 
     @Override
     public void run(String... args) throws Exception {
         Cliente c1 = new Cliente(null, "José", "jose@gmail.com", "123456789");
         Cliente c2 = new Cliente(null, "Maria", "maria@gmail.com", "987654321");
 
+        Pedido p1 = new Pedido(null, Instant.parse("2019-06-20T19:53:07Z"), c1);
+        Pedido p2 = new Pedido(null, Instant.parse("2019-07-21T03:42:10Z"), c2);
+        Pedido p3 = new Pedido(null, Instant.parse("2019-07-22T15:21:22Z"), c1);
+
         clienteRepository.saveAll(Arrays.asList(c1, c2));
+        pedidoRepository.saveAll(Arrays.asList(p1, p2, p3));
     }
 }
