602bd6994c03259ec10ba5dcd9dedb78f091e474
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-03-28 21:34:03 -0300

    implementando busca por id

--- backend/src/main/java/bean/backend/repository/ClienteRepository.java
+++ backend/src/main/java/bean/backend/repository/ClienteRepository.java
@@ -2,7 +2,9 @@ package bean.backend.repository;
 
 import bean.backend.entities.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
 }
--- backend/src/main/java/bean/backend/resource/ClienteResource.java
+++ backend/src/main/java/bean/backend/resource/ClienteResource.java
@@ -1,18 +1,32 @@
 package bean.backend.resource;
 
 import bean.backend.entities.Cliente;
+import bean.backend.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/clientes")
 public class ClienteResource {
 
+    @Autowired
+    private ClienteService service;
+
     @GetMapping
-    public ResponseEntity<Cliente> findAll() {
-        Cliente c = new Cliente(1L, "José", "jose@gmail.com", "123456789");
-        return ResponseEntity.ok().body(c);
+    public ResponseEntity<List<Cliente>> findAll() {
+        List<Cliente> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Cliente> findById(@PathVariable Long id) {
+        Cliente obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
