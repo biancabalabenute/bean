89220c0e905829ec929b3985ae1fb630ab11a696
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-03-30 11:12:47 -0300

    criando o repository do produto

---
+++ backend/src/main/java/bean/backend/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package bean.backend.repository;
+
+import bean.backend.entities.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
