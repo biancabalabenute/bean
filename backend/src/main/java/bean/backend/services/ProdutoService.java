a8b5d048487507e91862e73b3714fd08248bfa23
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-03-30 11:25:24 -0300

    criando service de produto

---
+++ backend/src/main/java/bean/backend/services/ProdutoService.java
@@ -0,0 +1,25 @@
+package bean.backend.services;
+
+import bean.backend.entities.Produto;
+import bean.backend.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository repository;
+
+    public List<Produto> findAll() {
+        return repository.findAll();
+    }
+
+    public Produto findById(Long id) {
+        Optional<Produto> obj = repository.findById(id);
+        return obj.get();
+    }
+}
