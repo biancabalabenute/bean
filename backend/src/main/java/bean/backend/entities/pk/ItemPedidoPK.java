85b60a1b224702e633d28c14210b01b6ceead75c
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-03-30 12:03:00 -0300

    criando classe auxiliar de chaves estrangeiras entre pedido e produto

---
+++ backend/src/main/java/bean/backend/entities/pk/ItemPedidoPK.java
@@ -0,0 +1,54 @@
+package bean.backend.entities.pk;
+
+import bean.backend.entities.Pedido;
+import bean.backend.entities.Produto;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class ItemPedidoPK implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ItemPedidoPK that = (ItemPedidoPK) o;
+        return Objects.equals(pedido, that.pedido) && Objects.equals(produto, that.produto);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pedido, produto);
+    }
+}
