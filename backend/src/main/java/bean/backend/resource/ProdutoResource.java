f477d6397e07b7eb4193e7c3a749df6905264bcd
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-03-30 11:26:34 -0300

    criando resource de produto

---
+++ backend/src/main/java/bean/backend/resource/ProdutoResource.java
@@ -0,0 +1,32 @@
+package bean.backend.resource;
+
+import bean.backend.entities.Produto;
+import bean.backend.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/produtos")
+public class ProdutoResource {
+
+    @Autowired
+    private ProdutoService service;
+
+    @GetMapping
+    public ResponseEntity<List<Produto>> findAll() {
+        List<Produto> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Produto> findById(@PathVariable Long id) {
+        Produto obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
