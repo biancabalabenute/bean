4734984ebbb2fc7b045088e579077b684b2b2d29
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-04-01 16:03:03 -0300

    criando repository estado

---
+++ backend/src/main/java/bean/backend/repository/EstadoRepository.java
@@ -0,0 +1,10 @@
+package bean.backend.repository;
+
+import bean.backend.entities.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Long> {
+
+}
