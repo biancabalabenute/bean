cdc6a129be34ea7186c0c5bfdee7b2d31c6837aa
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-04-01 15:55:06 -0300

    desenvolvendo e relacionando cidade e estado

--- backend/src/main/java/bean/backend/entities/Cliente.java
+++ backend/src/main/java/bean/backend/entities/Cliente.java
@@ -16,6 +16,7 @@ public class Cliente implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
