c89e7b5dcffdbc7109abd7dd774ca742bd8b5edd
Ana Guassu <annaguassu@gmail.com>
2024-05-03 11:51:58 -0300

    ajuste itemPedido Resource

--- backend/src/main/java/bean/backend/resource/ItemPedidoResource.java
+++ backend/src/main/java/bean/backend/resource/ItemPedidoResource.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/item-pedido")
+@RequestMapping("/itemPedido")
 public class ItemPedidoResource {
 
     @Autowired
