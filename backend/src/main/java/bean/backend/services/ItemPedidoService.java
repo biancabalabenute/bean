de364687ce4940f81fa89afd673982b3a0dc967f
Bianca Balabenute <balabenute.bianca@gmail.com>
2024-06-05 14:14:44 -0300

    ver itens pedidos

--- backend/src/main/java/bean/backend/services/ItemPedidoService.java
+++ backend/src/main/java/bean/backend/services/ItemPedidoService.java
@@ -1,14 +1,21 @@
 package bean.backend.services;
 
+import bean.backend.entities.ItemPedido;
 import bean.backend.repository.ItemPedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ItemPedidoService {
 
     @Autowired
     private ItemPedidoRepository itemPedidoRepository;
+
+    public List<ItemPedido> findAll() {
+        return itemPedidoRepository.findAll();
+    }
 
     public void deletarItemPedidoPorIdVenda(String idVendaPlataforma) {
         itemPedidoRepository.deleteByIdVendaPlataforma(idVendaPlataforma);
